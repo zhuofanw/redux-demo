385a0c391b6ddd04a1549f04fd131d941dcff7d9
zhuofanw <zhuofanw@sfu.ca>
2019-11-18 14:41:01 -0800

    react-redux connect() with first parameter

--- src/index.js
+++ src/index.js
@@ -5,26 +5,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers/counter';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
 
-// store.subscribe(() => console.log("State updated!", store.getState()));
-
-//
-// store.dispatch({type:"INCREMENT"});
-
-const render = () => {
-    ReactDOM.render(<App onIncrement={ () => store.dispatch({type:"INCREMENT"})}
-                         onDecrement={ () => store.dispatch({type:"DECREMENT"})}
-                         value={ store.getState() }
-    />, document.getElementById('root'));
-
-};
-
-render();
-
-store.subscribe(render);
-
+ReactDOM.render(
+        <Provider store={store}>
+        <App/>
+        </Provider>,
+        document.getElementById('root')
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,4 +1,4 @@
-const counter = (state = 0, action={}) =>{
+const counter = (state = 1, action={}) =>{
     switch (action.type) {
         case 'INCREMENT':
             return state + 1;
